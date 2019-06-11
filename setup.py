3eac90857d2b6283866bb0274dd08dcd1e6f661f
Jérôme BUISINE <contact@jeromebuisine.fr>
2019-06-11 16:18:49 +0200

    Update of documentation

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ class BuildTestCommand(setuptools.command.build_py.build_py):
 
 setup(
     name='ipfml',
-    version='0.3.7',
+    version='0.3.9',
     description='Image Processing For Machine Learning',
     long_description=readme(),
     classifiers=[
