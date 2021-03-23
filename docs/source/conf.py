1e48a4640e7145b446be89010ea19ac74f44357d
Jérôme BUISINE <contact@jeromebuisine.fr>
2021-03-23 17:03:07 +0100

    Gini Kernel added

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Jérôme BUISINE'
 author = 'Jérôme BUISINE'
 
 # The short X.Y version
-version = '1.0.3'
+version = '1.0.4'
 # The full version, including alpha/beta/rc tags
-release = 'v1.0.3'
+release = 'v1.0.4'
 
 
 # -- General configuration ---------------------------------------------------
